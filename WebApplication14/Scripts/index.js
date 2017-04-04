4af939f774bf6d4e85e908c9d89c462fa8e95aae
Avrumi Friedman <a.friedman07@gmail.com>
2017-04-03 21:06:10 -0400

    Initial commit

---
+++ WebApplication14/Scripts/index.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+    var number = 1;
+    $("#add-row").on('click', function () {
+        var firstNameTextBox = getInput('people[' + number + '].firstName', "First Name");
+        var lastNameTextBox = getInput('people[' + number + '].lastName', "Last Name");
+        var ageTextBox = getInput('people[' + number + '].age', "Age");
+        var row = $("<tr><td>" + firstNameTextBox + "</td><td>" + lastNameTextBox + "</td><td>"
+            + ageTextBox + "</td></tr>");
+        $("table").append(row);
+        number++;
+    });
+
+    function getInput(name, placeholder) {
+        return "<input type='text' placeholder='" + placeholder + "' name='" +
+            name + "' class='form-control' />";
+    }
+});
